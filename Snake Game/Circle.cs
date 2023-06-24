b6090aaaa171cbf489144416424b98aa67d92d98
olivia svensson <oliviateoliva@gmail.com>
2023-06-24 17:21:33 +0200

    creating methods

--- Snake Game/Circle.cs
+++ Snake Game/Circle.cs
@@ -8,5 +8,13 @@ namespace Snake_Game
 {
     internal class Circle
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Circle()
+        { 
+            X = 0;
+            Y = 0;
+        }
     }
 }
--- Snake Game/Form1.cs
+++ Snake Game/Form1.cs
@@ -1,25 +1,70 @@
+using System.Configuration;
+
 namespace Snake_Game
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+        int maxWidth;
+        int maxHeight;
+
+        int score;
+        int highScore;
+
+        Random rand = new Random(); 
+
+        bool goLeft, goRight, goUp, goDown; 
+
+
         public Form1()
         {
             InitializeComponent();
+            new Settings();
         }
 
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Left && Settings.directions != "right") {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right && Settings.directions != "left") {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && Settings.directions != "down") { 
+            goUp = true; 
+            }
+            if (e.KeyCode == Keys.Down && Settings.directions != "up") {
+                goDown = true;
+            }
 
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
 
         }
 
         private void StartGame(object sender, EventArgs e)
         {
-
+            RestartGame();
         }
 
         private void TakeSnapShot(object sender, EventArgs e)
@@ -38,8 +83,11 @@ namespace Snake_Game
         }
 
         private void RestartGame()
-        { 
+        {
+            maxWidth = gamebox.Width / Settings.Width - 1;
+            maxHeight = gamebox.Height / Settings.Height - 1;
 
+            Snake.Clear();
         }
 
         private void EatFood()
